8f1318bdd4b8df33bfa7c1fee67e0c319a799963
Leonardo Almeida Marques <leonardo.almeidamarques@finqle.nl>
2020-07-30 18:41:15 +0200

    chore: :white_check_mark: updated tests

--- packages/components/src/toaster/Toaster.test.tsx
+++ packages/components/src/toaster/Toaster.test.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer'; // ES6
 import Toaster from '.';
+import { StyledToaster } from './styles';
 
 test('Toaster scenarios', () => {
-  const defaultBtn = TestRenderer.create(<Toaster>Hello</Toaster>);
-  expect(defaultBtn).toMatchSnapshot();
+  const defaultToaster = TestRenderer.create(<Toaster>Hello</Toaster>);
+  expect(defaultToaster).toMatchSnapshot();
+
+  const defaultStyledToaster = TestRenderer.create(<StyledToaster>Styled toaster</StyledToaster>);
+  expect(defaultStyledToaster).toMatchSnapshot();
+
+  const defaultOpenStyledToaster = TestRenderer.create(<StyledToaster isOpen>Styled opened toaster</StyledToaster>);
+  expect(defaultOpenStyledToaster).toMatchSnapshot();
 });
